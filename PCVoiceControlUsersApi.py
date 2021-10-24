58d713dab88036f80d33f3fca97a957a3674ea4c
MichalC76 <michalc76@gmail.com>
2021-10-24 11:10:05 +0200

    up

---
+++ PCVoiceControlUsersApi.py
@@ -0,0 +1,21 @@
+import boto3
+ddb = boto3.client("dynamodb")
+
+def handler(event, context):
+    try:
+        data = ddb.put_item(
+            TableName="Users",
+            Item={
+                'email': {
+                    'S': event.email
+                },
+                'device': {
+                    'S': event.device
+                }
+            }
+        )
+    except BaseException as e:
+        print(e)
+        raise(e)
+    
+    return {"message": "Successfully Added"}
